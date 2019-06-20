bc8b7e1a9c33e2a4aa03382aad882b423943ed33
captsilver <davidgoree2003@gmail.com>
2019-06-20 11:40:43 -0500

    Added initial script for sakura.

---
+++ sakura_installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Need to review pre-reqs
+#Needs desktop-dev to compile, :(
+#Code has not been tested yet...
+
+echo "Downloading Sakura!"
+wget -P /tmp https://launchpad.net/sakura/trunk/3.6.0/+download/sakura-3.6.0.tar.bz2
+
+echo "Decompressing package"
+tar xvf /tmp/sakura-3.6.0.tar.bz2
+
+echo "Compiling and Installing Sakura!"
+pushd /tmp/sakura-3.6.0
+cmake ./
+make
+make install
+popd
+
+echo "Cleaning up"
+rm -rf /tmp/sakura*
